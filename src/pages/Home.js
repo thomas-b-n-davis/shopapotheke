22b8f3175dd9684593c38f79f45b9a4c7224a1c0
Thomas Davis <fusionelleconsult@gmail.com>
2022-04-26 13:43:18 +0200

    Language filter added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,18 +8,25 @@ export default class Home extends React.Component {
             list: [],
             loading: false,
             errorMsg: "",
-            filter:"all",
-            starred:[],
-            rating:[1,2,3,4,5]
+            filter: "all",
+            normalFilterList: ["all", "starred"],
+            starred: [],
+            rating: [1, 2, 3, 4, 5],
+            languages: []
         }
     }
     componentDidMount() {
         // this.loadData();
         let cached = JSON.parse(localStorage.getItem("items"));
-        this.setState({ list: cached });
+        var languages = cached.map(data => {
+            if (data.language != null)
+                return data.language;
+        });
+        languages = [...new Set(languages.map(item => item))];
+        this.setState({ list: cached, languages: languages });
 
         let starred = localStorage.getItem("starred");
-        starred=(starred!=null)?JSON.parse(starred):[];
+        starred = (starred != null) ? JSON.parse(starred) : [];
         this.setState({ starred: starred });
     }
 
@@ -34,7 +41,13 @@ export default class Home extends React.Component {
                     return Promise.reject(response.json());
             })
             .then(result => {
-                self.setState({ list: result.items });
+                var languages = result.items.map(data => {
+                    if (data.language != null)
+                        return data.language;
+                });
+                languages = [...new Set(languages.map(item => item))];
+                self.setState({ list: result.items, languages: languages });
+
                 localStorage.setItem("items", JSON.stringify(result.items));
             }).catch(err => {
                 err.then(error => {
@@ -45,23 +58,26 @@ export default class Home extends React.Component {
             });
     }
 
-    starItem=(id,star)=>{
-        let starred=this.state.starred;
-        if(starred.filter(data=>(data.id==id)).length>0){
-            starred=starred.filter(data=>(data.id!=id));
+
+    starItem = (id, star) => {
+        let starred = this.state.starred;
+        if (starred.filter(data => (data.id == id)).length > 0) {
+            starred = starred.filter(data => (data.id != id));
         }
-        starred.push({id:id,rating:star});
-        localStorage.setItem("starred",JSON.stringify(starred));
-        this.setState({starred:starred});
+        starred.push({ id: id, rating: star });
+        localStorage.setItem("starred", JSON.stringify(starred));
+        this.setState({ starred: starred });
     }
 
 
     render() {
-        const { list, loading, errorMsg,starred,filter,rating } = this.state;
+        const { list, loading, errorMsg, starred, filter, rating, languages, normalFilterList } = this.state;
         let itemList = list;
 
-        if(filter!=="all"){
-            itemList=list.filter(data=>(starred.filter(star=>(star.id==data.id)).length>0));
+        if (filter === "starred") {
+            itemList = list.filter(data => (starred.filter(star => (star.id == data.id)).length > 0));
+        } else if (filter !== "all" && filter !== "starred") {
+            itemList = list.filter(data => (data.language == filter));
         }
         return (<>
             <div className="container">
@@ -76,11 +92,15 @@ export default class Home extends React.Component {
                         {(loading == true) && <div className="col-12 text-center"><Spinner fill="#000" bg="#fff" /></div>}
                         {(errorMsg != "") && <div className="col-12 text-center"><small>{errorMsg}</small></div>}
 
-                        <div class="input-group mb-3">
-                            
-                            <button class="btn btn-outline-secondary" type="button" onClick={()=>this.setState({filter:"all"})}>Show All</button>
-                            <button class="btn btn-outline-secondary" type="button" onClick={()=>this.setState({filter:"starred"})}>Show Starred</button>
+                        <small>Normal Filters</small>
+                        <div class="input-group mb-3 filters">
+                            {normalFilterList.map(filters => <button class={(filter == filters) ? "btn btn-secondary" : "btn btn-outline-secondary"} type="button" onClick={() => this.setState({ filter: filters })}>{filters}</button>)}
                         </div>
+                        <small>Language Filters</small>
+                        {(languages.length > 0) &&
+                            <div class="input-group mb-3 filters">
+                                {languages.map(lang => <button class={(filter == lang) ? "btn btn-secondary" : "btn btn-outline-secondary"} type="button" onClick={() => this.setState({ filter: lang })}>{lang}</button>)}
+                            </div>}
                         <table>
                             <thead>
                                 <td>Name</td>
@@ -90,15 +110,15 @@ export default class Home extends React.Component {
                                 <td>Starring</td>
                             </thead>
                             {itemList.map(items => {
-                                var rate=starred.filter(star=>(star.id==items.id));
-                                rate=(rate.length>0)?rate[0].rating:0;
+                                var rate = starred.filter(star => (star.id == items.id));
+                                rate = (rate.length > 0) ? rate[0].rating : 0;
                                 return <tr className="col-12 item-container">
                                     <td>{items.name}{JSON.stringify(rate)}</td>
                                     <td>{items.description}</td>
                                     <td>{items.stargazers_count}</td>
                                     <td><a href={items.url} target="_blank"><ion-icon name="link"></ion-icon></a></td>
-                                    <td>{rating.map(stars=>{
-                                        return <button onClick={()=>this.starItem(items.id,stars)}><ion-icon name={(rate>=stars)?"star":"star-outline"}></ion-icon></button>
+                                    <td>{rating.map(stars => {
+                                        return <button onClick={() => this.starItem(items.id, stars)}><ion-icon name={(rate >= stars) ? "star" : "star-outline"}></ion-icon></button>
                                     })}</td>
                                 </tr>
                             })}
